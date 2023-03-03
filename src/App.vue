19ee650a2c90cd1b04ebc94ab04b2b42f6e8b6a0
jacob-d242 <jacobmoracha@gmail.com>
2023-03-03 09:55:39 +0300

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
@@ -33,7 +32,6 @@ header {
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
-
   .logo {
     margin: 0 2rem 0 0;
   }
